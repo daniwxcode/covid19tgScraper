e3ef4a68c63fec34967e0fe28ea011b605395566
Daniel WOAGOU <daniwx@gmail.com>
2020-04-30 16:58:10 +0000

    AutoUpdate

---
+++ Data/InfosCovidProvider.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using AngleSharp;
+using covid19tg_scraper.Models;
+using covid19tg_scraper.Services;
+using Newtonsoft.Json;
+
+namespace covid19tg_scraper.Data
+{
+    public static class InfosCovidProvider
+    {
+        public static bool IsUpdate{get;set;}
+        public static List<Details> Details { get; set; }
+        public static Timer AutoRefreshTimer { get; set; }
+        public static Stats Stats { get; set; }
+        public static void AutoRefresh()
+        {
+            IsUpdate =false;
+            var startTimeSpan = TimeSpan.Zero;
+            var periodTimeSpan = TimeSpan.FromMinutes(10);
+
+            AutoRefreshTimer = new Timer(async (e) =>
+            {
+                var newData = await GetDetailsAsync();
+                newData.Reverse();
+                foreach (var tmp in newData)
+                {
+                    if(Details ==null) Details = newData;
+                    if (Details.All(p => p.Stat.TimeInfo != tmp.Stat.TimeInfo))
+                    {
+                        Details.Insert(0, tmp);         
+                        IsUpdate =true;
+                    }
+
+                }
+                 Stats = new Stats(await GetStatAsync());
+ 
+             
+      
+            }, null, startTimeSpan, periodTimeSpan);
+
+        }
+
+        public  static async Task<List<Details>> GetDetailsAsync()
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync("https://covid19.gouv.tg/situation-au-togo/");
+            var details = new List<Details>();
+
+            var sections = document.QuerySelectorAll(".ee-loop__item>article>div>div>div");
+            string xt = string.Empty;
+
+            foreach (var item in sections.Skip(1))
+            {
+                var itemDetails = new Details();
+                xt += "\n\n";
+                var itemsections = item.QuerySelectorAll("section");
+                var itemHtmlDetails = itemsections.FirstOrDefault().QuerySelectorAll("h2");
+
+
+                int i = 0;
+                Stats itemStats = new Stats();
+                itemStats.TimeInfo = $"{itemHtmlDetails[0].InnerHtml} à { itemHtmlDetails[1].InnerHtml}";
+                itemStats.ActiveCases = itemHtmlDetails[3].InnerHtml.GetInt();
+                itemStats.Cured = itemHtmlDetails[4].InnerHtml.GetInt();
+                itemStats.Deaths = itemHtmlDetails[5].InnerHtml.GetInt();
+                itemDetails.Stat = itemStats;
+
+                //    itemDetails.Histoire = Regex.Replace(itemsections[1].TextContent, @"^[\s\t\n]+|[\s\t\n]+$", "\n");
+                itemDetails.Histoire = Regex.Replace(itemsections[1].TextContent, @"<[^>]+>|&nbsp;", " ").Trim();
+                itemDetails.Histoire = Regex.Replace(itemsections[1].TextContent, @"U", "  U").Trim();
+
+                itemDetails.Histoire = Regex.Replace(itemDetails.Histoire, @"\s{2,}", " ");
+                itemDetails.Histoire = Regex.Replace(itemDetails.Histoire, @"\n{2,}", "\n");
+                itemDetails.Histoire = Regex.Replace(itemDetails.Histoire, @"\t{3,}", "\t\t"); ;
+                details.Add(itemDetails);
+
+            }
+
+            return details;
+        }
+
+        public static async Task<Stat> GetStatAsync()
+        {
+            Stat details;
+
+            using (var httpClient = new HttpClient())
+            {
+                var json = await httpClient.GetStringAsync("https://coronavirus-tg-api.herokuapp.com/v1/cases");
+                details = JsonConvert.DeserializeObject<Stat>(json);
+                return details;
+            }
+
+        }
+    }
+}
--- Models/Stats.cs
+++ Models/Stats.cs
@@ -5,18 +5,37 @@ using System.Threading.Tasks;
 
 namespace covid19tg_scraper.Models
 {
-    public class Stats
+     public class Stats
     {
         public uint ActiveCases { get; set; }
         public uint Cured { get; set; }
         public uint Deaths { get; set; }
+        public string TimeInfo { get; set; }
+
         public uint Total { get { return ActiveCases + Cured + Deaths; } }
-
-        public string TimeInfo { get;  set; }
-
         public override string ToString()
         {
             return $"\n Actifs= {ActiveCases} -Guéris={Cured} - Décès ={Deaths} - Total ={Total}";
         }
+        public Stats()
+        {
+
+        }
+        public Stats(Stat stat)
+        {
+            Cured =uint.Parse(stat.Recovered);
+            Deaths = uint.Parse(stat.Deaths);
+            ActiveCases = uint.Parse(stat.Confirmed)-(Cured+Deaths);
+            TimeInfo =stat.Last_updated;
+        }
     }
+    
+    public class Stat
+    {
+        public string Confirmed{get; set; }
+        public string Recovered{get;set; }
+        public string Deaths{get;set; }
+        public string Last_updated{get;set;}
+    }
+
 }
